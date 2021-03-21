7348fa2d4aa73966a239577a78c64a8da99ae9f5
ethanjd23 <ethanjd23@gmail.com>
2021-03-21 12:46:44 -0500

    backend updated to give front-end easier data to work with

--- src/server/db/chirpsdb.ts
+++ src/server/db/chirpsdb.ts
@@ -1,8 +1,18 @@
 import { query } from './index';
 
-const getAll = async () => query("SELECT * FROM chirps")
+const getAll = async () => query(`SELECT users.name, chirps.content, chirps.location, GROUP_CONCAT(mentions.userid) AS mentionedUsersIDs
+                                    FROM chirps
+                                    INNER JOIN users ON chirps.userid=users.id
+                                    LEFT JOIN mentions ON chirps.id = mentions.chirpid
+                                    GROUP BY chirps.id
+                                    ORDER BY chirps.id`);
 
-const getOne = async (id: number) => query(`SELECT * FROM chirps WHERE id = ?`, [id])
+const getOne = async (id: number) => query(`SELECT users.name, chirps.content, chirps.location, GROUP_CONCAT(mentions.userid) AS mentionedUsersIDs
+                                            FROM chirps
+                                            INNER JOIN users ON chirps.userid=users.id
+                                            LEFT JOIN mentions ON chirps.id = mentions.chirpid
+                                            WHERE chirps.id = ?
+                                            ORDER BY chirps.id`, [id])
 
 const deleteMention = async (id: number) => query(`DELETE FROM mentions WHERE chirpid = ?`, [id])
 const deleteChirp = async (id: number) => query(`DELETE FROM chirps WHERE id = ?`, [id])
@@ -11,6 +21,7 @@ const updateChirp = async (id: number, content: string, location: string) => que
 
 const createChirp = async (userid: number, content: string, location: string) => query(`INSERT INTO chirps (userid, content, location) VALUES (?, ?, ?)`, [userid, content, location]);
 const createMention = async (chirpid: number, mentionedUserid: number) => query(`INSERT INTO mentions VALUES (?, ?)`, [mentionedUserid, chirpid])
+
 const getNewestChirpID = async () => query(`SELECT MAX(id) FROM chirps`)
 
 export default {
